3e9c746874d744c9665e697d99351771fe432162
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-21 18:22:55 +0100

    add text and link mapguess and quizz

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,9 @@
-const projects = []
+import mapguess from "./img/mapguess.png"
+import quizz from "./img/quizz.png"
 
+
+
+const projects = []
 
 // const {name,image,text,ghLink,liveLink}= props
 
@@ -10,15 +14,12 @@ const newProject = (name,image,text,ghLink,liveLink) => {
 }
 
 
-const project1 = newProject("Map Guess","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
-const project2 = newProject("QuizzIt","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
+const project1 = newProject("Map Guess",mapguess,"Makiere die gesuchten Dinge auf einem Foto. Lade selbst ein Bild hoch, makiere die Flächen und teile es mit anderen.", "https://github.com/feindreh/mapguess", "https://feindreh.github.io/mapguess/" )
+const project2 = newProject("QuizzIt",quizz,"Beantworte die Fragen in einem Quiz. Erstelle selbst ein Quiz und teile es mit Freunden.", "Link-Gh", "Link-Live" )
 const project3 = newProject("ShoppingCart","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
-const project4 = newProject("Miss1","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
-const project5 = newProject("Miss2","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
-const project6 = newProject("Miss3","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
+const project4 = newProject("Api","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
 
-projects.push(project1,project2,project3,project4,project5,project6)
+projects.push(project1,project2,project3,project4)
 
-console.log(projects)
 
 export default projects
