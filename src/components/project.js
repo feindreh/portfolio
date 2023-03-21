a223450f92da6f9ffc631ef4a6d0993eefc69168
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-21 16:12:08 +0100

    add Project component

---
+++ src/components/project.js
@@ -0,0 +1,12 @@
+function Project(props){
+
+    const {name,image,text,ghLink,liveLink}= props
+
+    return(
+        <div className="project">
+            Project
+        </div>
+    )
+}
+
+export default Project
