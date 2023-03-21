93f6a036adc05a5f84c437754e025b9268f445de
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-21 16:12:22 +0100

    add Main content

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,21 @@
 import "./main.css"
 
+import Project from "./project"
+import projects from "../projects"
+
 function Main(){
+
+
+
     return(
-        <div id="main">Main</div>
+        <div id="main">
+            <h1 id="work">My Work</h1>
+            <div id="projects">
+                {projects.map((project) => {
+                    return <Project key = {project.name} project={project}/>
+                })}
+            </div>
+        </div>
     )
 }
 
