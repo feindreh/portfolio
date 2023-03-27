101bc809da8c16b7c1c0ca2d9bcaa1ba573da54a
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-27 20:01:56 +0200

    change order

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,6 @@ import quizz from "./img/quizz.png"
 
 const projects = []
 
-// const {name,image,text,ghLink,liveLink}= props
-
 const newProject = (name,image,text,ghLink,liveLink) => {
     return {
         name,image,text,ghLink,liveLink
@@ -15,9 +13,11 @@ const newProject = (name,image,text,ghLink,liveLink) => {
 
 
 const project1 = newProject("Map Guess",mapguess,"Makiere die gesuchten Dinge auf einem Foto. Lade selbst ein Bild hoch, makiere die Flächen und teile es mit anderen.", "https://github.com/feindreh/mapguess", "https://feindreh.github.io/mapguess/" )
-const project2 = newProject("QuizzIt",quizz,"Beantworte die Fragen in einem Quiz. Erstelle selbst ein Quiz und teile es mit Freunden.", "Link-Gh", "Link-Live" )
-const project3 = newProject("ShoppingCart","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
-const project4 = newProject("Api","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
+const project2 = newProject("QuizzIt",quizz,"Beantworte die Fragen in einem Quiz. Erstelle selbst ein Quiz und teile es mit Freunden.", "https://github.com/feindreh/quizz-it", "https://feindreh.github.io/quizz-it/" )
+
+const project3 = newProject("Api","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
+
+const project4 = newProject("ShoppingCart","Screenshot","Karten raten macht Spaß", "Link-Gh", "Link-Live" )
 
 projects.push(project1,project2,project3,project4)
 
